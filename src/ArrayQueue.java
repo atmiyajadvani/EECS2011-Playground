75809a37777068e7778a28cc135b5827d8e0364d
Atmiya Jadvani <atmiya@my.yorku.ca>
2023-03-12 16:44:28 -0400

    Implemenet Queue ADT with Array

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,53 +1,63 @@
-//import java.util.Queue;
-//
-//public class ArrayQueue<E> implements Queue<E> {
-//
-//    // instance variables
-//    private E[] data;        //generic array for storage
-//    private int f = 0;       // index of the front element
-//    private int sz = 0;      // current no of elements
-//
-//    // queue constructor
-//    public ArrayQueue(){
-//        this(CAPACITY);
-//    }      // constructs queue with default capacity
-//    public ArrayQueue(int CAPACITY){             // constructs queue with given capacity
-//        data = (E[]) new Object[CAPACITY];       // safe castinng
-//    }
-//
-//    // methods
-//    public int size(){ return sz;}               // returns no of element in the queue
-//    public boolean isEmpty(){                    // checks if the queue is empty
-//        return (sz == 0);
-//    }
-//
-//    // inserts an element at the rear (i.e. back) of the queue
-//    public void enqueue(E e) throws IllegalStateException{
-//        if(sz == data.length) throw new IllegalStateException("Queue is full");
-//        int avail = (f + sz) % data.length;   // use of modular arithmetic
-//        data[avail] = e;
-//        sz++;
-//    }
-//
-//    // returns the first element of the queue
-//    public E first(){
-//        if (isEmpty())
-//            return null;
-//        return data[f];
-//    }
-//
-//    // removes and returns the first element of the queue
-//    public E dequeue(){
-//        if(isEmpty())
-//            return null;
-//        E answer = data[f];
-//        data[f] = null;           // deference to help garbage collection
-//        f = (f + 1) % data.length;
-//        sz--;
-//        return answer;
-//    }
-//
-//
-//
-//
-//}
+import java.util.Arrays;
+import java.util.Queue;
+
+// Implementing the Queue ADT using a fixed length array
+public class ArrayQueue<E>{
+
+    // instance variables
+    private E[] arrayQueue;
+    int indexOfFirstElement = 0;
+    int noOfElements = 0;
+
+    // Constructor
+    public ArrayQueue(int capacity){
+        arrayQueue = (E[]) new Object[capacity];
+    }
+
+    // Essential Methods -------------------------
+
+    // Returns the size of the queue
+    public int size(){
+        return noOfElements;
+    }
+
+    // Returns the true if the queue is empty
+    public boolean isEmpty(){
+        return (noOfElements == 0);
+    }
+
+    // Returns the first element of queue without removing it
+    public E front(){
+        if (isEmpty()) {
+            return null;
+        }
+        return arrayQueue[indexOfFirstElement];
+    }
+
+    // Inserts an element at the end of the queue
+    public void enqueue(E e) throws IllegalStateException{
+
+        // check if the array is full capacity
+        if (noOfElements == arrayQueue.length) {
+            throw new IllegalStateException("Queue is full");
+        }
+
+        int available = (noOfElements + indexOfFirstElement) % arrayQueue.length;
+        arrayQueue[available] = e;
+        noOfElements++;
+    }
+
+    // Removes and returns the first elements of the queue
+    public E dequeue(){
+        if (isEmpty()) {
+            return null;
+        }
+
+        E answer = arrayQueue[indexOfFirstElement];
+        arrayQueue[indexOfFirstElement] = null;
+        indexOfFirstElement = (indexOfFirstElement + 1) % 10;
+        noOfElements--;
+        return answer;
+    }
+
+}
