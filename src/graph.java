57b7655b7da5cf9b0ded942d39e29885b3221c01
Atmiya Jadvani <atmiya@my.yorku.ca>
2023-03-22 23:03:43 -0400

    heading to bed

---
+++ src/graph.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ *
+ *
+ */
+
+
+public class graph {
+
+    public static void main(String[] args){
+    }
+
+
+    ArrayList<LinkedList<graphNode>> alist;
+    public void Graph(){
+        alist = new ArrayList<>();
+    }
+
+    public void addNode(graphNode node){
+
+    }
+
+    public void addEdge(int source, int destination){
+
+    }
+
+    public void checkEdge(int source, int destination){
+
+    }
+
+    public void print(){
+
+    }
+
+}
+
+class graphNode{
+    char data;
+    public void Node(char data){
+        this.data = data;
+    }
+
+}
