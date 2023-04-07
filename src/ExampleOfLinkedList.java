a313bc851aa711b8ca5c86a95425c87bd56c18cb
Atmiya Jadvani <atmiya@my.yorku.ca>
2023-04-07 19:31:32 -0400

    saving

---
+++ src/ExampleOfLinkedList.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class ExampleOfLinkedList
+{
+    public static void main(String[] args)
+    {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        int listPointer;
+        int ref1;
+        int ref2;
+
+        list.add(25);
+        list.add(30);
+        list.add(45);
+        list.add(60);
+        list.add(65);
+        list.add(80);
+        list.add(90);
+
+        listPointer = list.get(0);
+        ref1 = list.get(1);
+        ref2 = list.get(5);
+
+        list = list.
+
+        System.out.println();
+    }
+}
